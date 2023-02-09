bf1b28caf570487966a5882666bdc2b556594354
Tinydude153 <42678254+Tinydude153@users.noreply.github.com>
2023-02-09 17:37:49 -0500

    Update getBookmarks.ps1

--- getBookmarks.ps1
+++ getBookmarks.ps1
@@ -2,15 +2,15 @@
     [CmdletBinding()]
     Param(
     [Parameter(Mandatory=$true)]
-    [string[]]$ComputerName,
-    [string[]]$UserName
+    [string]$ComputerName,
+    [string]$UserName
     )
 
     Process {
         $cred = Get-Credential
         $fullName = $ComputerName + ".holzer.org"
         $session = New-PSSession -ComputerName $ComputerName -Credential $cred
-        [string[]]$path = "C:\Users\" + $userName + "\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+        [string]$path = "C:\Users\" + $userName + "\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
         $checkPath = Test-Path -Path $path
         if ($checkPath -eq $True) {
             Copy-Item -Path $path -Destination "C:\Intel" -FromSession $session
