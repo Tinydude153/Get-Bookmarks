3168a75c12ec836428f1bb1573f8cd8d54cc7cf7
Tinydude153 <42678254+Tinydude153@users.noreply.github.com>
2023-02-09 17:13:46 -0500

    Add files via upload

---
+++ getBookmarks.ps1
@@ -0,0 +1,19 @@
+﻿function Get-Bookmarks {
+    [CmdletBinding()]
+    Param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$ComputerName,
+    [string[]]$UserName
+    )
+
+    Process {
+        $cred = Get-Credential
+        $fullName = $ComputerName + ".holzer.org"
+        $session = New-PSSession -ComputerName $ComputerName -Credential $cred
+        [string[]]$path = "C:\Users\" + $userName + "\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+        $checkPath = Test-Path -Path $path
+        if ($checkPath -eq $True) {
+            Copy-Item -Path $path -Destination "C:\Intel" -FromSession $session
+        }
+    }
+}
